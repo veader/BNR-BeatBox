edbb7be6790de72b382d09dc3710238a13905389
Shawn Veader <shawn@veader.org>
2015-08-27 09:05:17 -0700

    Chapter 18: Finished

---
+++ app/src/main/java/com/sjm/cardiomems/android/beatbox/BeatBoxActivity.java
@@ -0,0 +1,14 @@
+package com.sjm.cardiomems.android.beatbox;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
